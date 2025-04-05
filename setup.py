5bc919876897490ada2b765ffed9beeaed4c2dd5
Hufsa Haq <hufsahaq@gmail.com>
2025-04-05 11:53:31 +0100

    basic setup from pingproxy github

---
+++ setup.py
@@ -0,0 +1,37 @@
+import requests
+import sys
+import json
+
+def create_team(team_name: str, team_email: str) -> str:
+    r = requests.post(
+        "https://api.scrapemequickly.com/register",
+        data=json.dumps({"team_name": team_name, "team_email": team_email}),
+        headers={"Content-Type": "application/json"}
+    )
+
+    if r.status_code != 200:
+        print(r.json())
+        print("Failed to create a team")
+        sys.exit(1)
+
+    return r.json()["data"]["team_id"]
+
+def start_scraping_run(team_id: str) -> str:
+    r = requests.post(f"https://api.scrapemequickly.com/scraping-run?team_id={team_id}")
+
+    if r.status_code != 200:
+        print(r.json())
+        print("Failed to start scraping run")
+        sys.exit(1)
+
+    return r.json()["data"]["scraping_run_id"]
+
+def start_scraping_run(team_id: str) -> str:
+    r = requests.post(f"https://api.scrapemequickly.com/scraping-run?team_id={team_id}")
+
+    if r.status_code != 200:
+        print(r.json())
+        print("Failed to start scraping run")
+        sys.exit(1)
+
+    return r.json()["data"]["scraping_run_id"]
