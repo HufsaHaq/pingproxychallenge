ab59c67a807ef8493b119692df08ef78a84ffca0
Hufsa Haq <hufsahaq@gmail.com>
2025-04-05 12:24:36 +0100

    year price make model found

--- scraper.py
+++ scraper.py
@@ -5,6 +5,13 @@ for num in range(100000):
     URL = f"https://scrapemequickly.com/cars/static/{num}?scraping_run_id=89d5dca4-0a34-11f0-b686-4a33b21d14f6"
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
+    year = soup.find("p", class_ = "year")
+    price = soup.find("p", class_ = "price")
+    make = soup.find("h2", class_ = "title")
+    start = year.text.find("<strong>") + 1
+    end = year.text.find("</strong>")
+    print(year[start:end])
+    print(price.text)
+    print(make.text)
+    inputt = input()
     info.append(soup)
-
-    print(soup)
