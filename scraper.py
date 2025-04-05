4268b6c68977c7f66e159e8a0085b5f70c2eaf13
Liam A. Belbin <liam.belbin@student.manchester.ac.uk>
2025-04-05 13:43:28 +0100

    Fixed Mode

--- scraper.py
+++ scraper.py
@@ -5,10 +5,9 @@ def solution():
     price_total = 0
     max_year = 0
     min_year = 10000000
-    make_list = []
-    notfound = False
+    make_dict = {}
 
-    for num in range(99999):
+    for num in range(20):
         URL = f"https://scrapemequickly.com/cars/static/{num}?scraping_run_id=89d5dca4-0a34-11f0-b686-4a33b21d14f6"
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, "html.parser")
@@ -25,24 +24,14 @@ def solution():
         make = make_lst[0].replace(',','')
         if min_year > int(year_lst[1]):
             min_year = int(year_lst[1])
+        
+        if make not in make_dict:
+            make_dict[make] = 0
+        
+        make_dict[make] += 1
 
-        for i in make_list:
-            if make == i[0]:
-                i[1] += 1
-                break
-            else:
-                notfound = True
-        if notfound:
-            make_list.append([make, 1])
-        notfound = False
+    mode = max(make_dict, key=make_dict.get)
 
-    temp = 0
-    mode = ''
-    for i in make_list:
-        if i[1] > temp:
-            temp = i[1]
-            mode = i[0]
-    print("hi")
     print({"min_year": min_year,"max_year": max_year,"avg_price": price_total/99999,"mode_make": mode})
 
     return {
