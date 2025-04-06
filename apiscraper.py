824c10cde4fd68cf3666a87ec69c4a7898c4c93a
Hufsa Haq <hufsahaq@gmail.com>
2025-04-06 01:39:39 +0100

    Average price wrong

--- apiscraper.py
+++ apiscraper.py
@@ -16,7 +16,7 @@ def start_scraping_run(team_id: str) -> str:
     return r.json()["data"]["scraping_run_id"]
 
 
-THREAD_COUNT = 100
+THREAD_COUNT = 4
 TARGET = 25000
 RUN_ID = ""
 #run_ID = start_scraping_run('bace025d-120a-11f0-aaf0-0242ac120002')
@@ -26,6 +26,7 @@ make_dict = {}
 total = 0
 mode = 0
 store = []
+
 
 def get_key(id):
     ENDPOINT = f"https://api.scrapemequickly.com/get-token?scraping_run_id={id}"
@@ -39,27 +40,31 @@ def scraper(run_id):
     ENDPOINT = f"https://api.scrapemequickly.com/cars/test?scraping_run_id={run_id}&per_page=25&start=" #CONCAT THING AT END PLS
     threads = []
     for i in range(THREAD_COUNT):
-        threads.append(threading.Thread(target=lambda:get_data(ENDPOINT + str(int(10*i*(TARGET/THREAD_COUNT))), token)))
+        threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, i , token)))
     for thread in threads:
         thread.start()
     
     for t in threads:
         t.join()
 
-    process_data(store)
-    print({"min_year": min_year,"max_year": max_year,"avg_price": total/TARGET,"mode_make": mode})
+    result = process_data(store)
+    print(result)
+    print(len(store))
+    return (result)
 
     
         
-
-def get_data(endpoint, key):
+store_lock = threading.Lock()
+def get_data(endpoint, index , key):
+    global store
     users =["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36", "Mozilla/5.0 (Linux; Android 13; SM-S908B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 13; SM-S908U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36"]
     
-    for i in range(10):
+    for i in range(int(TARGET/(THREAD_COUNT*25))):
         success = False
         while not success:
             number = random.randint(0,2)
-            response = requests.get(endpoint, 
+            print(str(index+(25*i)))
+            response = requests.get(endpoint + str(index+(25*i)), 
                 headers={
                     "Authorization": f"Bearer {key}",
                     "User-Agent": users[number]
@@ -68,27 +73,27 @@ def get_data(endpoint, key):
             response = response.content
             response = response.decode("utf-8")
             response = json.loads(response)
-            print(response)
             if status != 429:
                 success = True
-                store.append(response["data"])
+                with store_lock:
+                    for j in range(25):
+                        store.append(response["data"][j])
             else:
-                print(response)
-                time.sleep(2)
+                print(status)
+            time.sleep(0.3)
 def process_data(store):
     global min_year, max_year, make_dict, total, mode
-    for response in store:
-        for car in response:
-            total += car["price"]
-            if car["year"]> max_year:
-                max_year = car["year"]
-            if car["year"] < min_year:
-                min_year = car["year"]
-                
-            if car["make"] not in make_dict:
-                make_dict[car["make"]] = 0
-        
-            make_dict[car["make"]] += 1
+    for car in store:
+        total += car["price"]
+        if car["year"]> max_year:
+            max_year = car["year"]
+        if car["year"] < min_year:
+            min_year = car["year"]
+            
+        if car["make"] not in make_dict:
+            make_dict[car["make"]] = 0
+    
+        make_dict[car["make"]] += 1
     
     mode = max(make_dict, key=make_dict.get)
     return {
@@ -98,10 +103,23 @@ def process_data(store):
         "mode_make": mode
     }
             
-    
+def submit(answers: dict, scraping_run_id: str) -> bool:
+    r = requests.post(
+        f"https://api.scrapemequickly.com/cars/solve?scraping_run_id={scraping_run_id}",
+        data=json.dumps(answers),
+        headers={"Content-Type": "application/json"}
+    )
+
+    if r.status_code != 200:
+        print(r.json())
+        print("Failed to submit answers")
+        return False
+
+    return True   
     
 
 run_ID = start_scraping_run('bace025d-120a-11f0-aaf0-0242ac120002')
 #run_ID ='d8559512-120b-11f0-b749-0242ac120003'
-print(scraper(run_ID))
-mode = max(make_dict, key=make_dict.get)
+#print(scraper(run_ID))
+#mode = max(make_dict, key=make_dict.get)
+submit(scraper(run_ID), run_ID)
