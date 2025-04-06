9b7c60e2706e2f7ca76495638ccc50717b5ccc27
Hufsa Haq <hufsahaq@gmail.com>
2025-04-06 03:39:47 +0100

    Proxy Added Successfully

--- apiscraper.py
+++ apiscraper.py
@@ -4,8 +4,9 @@ import sys
 import json
 import random
 import time
+from itertools import cycle
 
-THREAD_COUNT = 5
+THREAD_COUNT = 20
 TARGET = 25000
 RUN_ID = ""
 #run_ID = start_scraping_run('bace025d-120a-11f0-aaf0-0242ac120002')
@@ -15,6 +16,15 @@ make_dict = {}
 total = 0
 mode = 0
 store = []
+
+proxy_pool = cycle([
+    'http://pingproxies:scrapemequickly@194.87.135.1:9875',    
+    'http://pingproxies:scrapemequickly@194.87.135.2:9875',
+    'http://pingproxies:scrapemequickly@194.87.135.3:9875',
+    'http://pingproxies:scrapemequickly@194.87.135.4:9875',
+    'http://pingproxies:scrapemequickly@194.87.135.5:9875',
+])
+
 
 def start_scraping_run(team_id: str) -> str:
     r = requests.post(f"https://api.scrapemequickly.com/scraping-run?team_id={team_id}")
@@ -39,11 +49,26 @@ def scraper(run_id):
     ENDPOINT = f"https://api.scrapemequickly.com/cars/test?scraping_run_id={run_id}&per_page=25&start=" #CONCAT THING AT END PLS
     threads = []
 
-    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 0 , token)))
-    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 1 , token)))
-    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 2 , token)))
-    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 3 , token)))
-    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 4 , token)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 0 , token, False)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 1 , token, False)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 2 , token, False)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 3 , token, False)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 4 , token, False)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 5 , token, False)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 6 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 7 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 8 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 9 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 10 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 11, token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 12 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 13 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 14 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 15 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 16 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 17 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 19 , token, True)))
+    threads.append(threading.Thread(target=lambda:get_data(ENDPOINT, 18 , token, True)))
     for thread in threads:
         thread.start()
     
@@ -58,7 +83,7 @@ def scraper(run_id):
     
         
 store_lock = threading.Lock()
-def get_data(endpoint, index , key):
+def get_data(endpoint, index , key, is_proxy):
     global store
     users =["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36", "Mozilla/5.0 (Linux; Android 13; SM-S908B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 13; SM-S908U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36"]
     
@@ -69,11 +94,18 @@ def get_data(endpoint, index , key):
             #print(str((index*6250)+(25*i)))
             print('i',i)
             print('index',index)
-            response = requests.get(endpoint + str((index*5000)+(25*i)), 
+            proxy = next(proxy_pool)
+            response = requests.get(endpoint + str((index*1250)+(25*i)), 
                 headers={
                     "Authorization": f"Bearer {key}",
                     "User-Agent": users[number]
-                })
+                },
+                proxies = None if not is_proxy else {
+                    'http' : proxy,    
+                    'https' : proxy
+                } 
+            )
+            
             status = response.status_code
             response = response.content
             response = response.decode("utf-8")
@@ -85,7 +117,7 @@ def get_data(endpoint, index , key):
                         store.append(response["data"][j])
             else:
                 print(status)
-            time.sleep(0.2)
+
 def process_data(store):
     global min_year, max_year, make_dict, total, mode
     for car in store:
