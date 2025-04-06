4de6ed65d51fa4bbdac53de6f9700e8ef7032ba2
Hufsa Haq <hufsahaq@gmail.com>
2025-04-06 08:59:23 +0100

    update java conversion

--- Scraper.java
+++ Scraper.java
@@ -1,12 +1,12 @@
-import java.io.IOException;
+import java.util.*;
 import java.net.*;
-import java.net.http.HttpClient;
+import java.io.IOException;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.*;
+import java.net.http.HttpClient;
 
 
-public class Scraper{
+public class Scraper {
     private static final int THREAD_COUNT = 10;
     private static final int TARGET = 25000;
 
@@ -14,11 +14,11 @@ public class Scraper{
 
     private static int minYear = 50000;
     private static int maxYear = 0;
-    private static Map<String, Integer> makeDict = new HashMap<>();
+    private static Map<String, int> makeDict = new HashMap<>();
     private static int total = 0;
     private static String mode = "";
 
-    private static List<String> store = new ArrayList<>();
+    private static List<Objects> store = Collections.synchronizedList(new ArrayList());
 
     private static final List<String> proxyPool = Arrays.asList(
         "http://pingproxies:scrapemequickly@194.87.135.1:9875",
@@ -28,6 +28,7 @@ public class Scraper{
         "http://pingproxies:scrapemequickly@194.87.135.5:9875"
     );
 
+    Iterator<String> proxy_i = proxyPool.iterator();
     
     public static void main(String[] args) throws Exception {
         runId = startScrapingRun("bace025d-120a-11f0-aaf0-0242ac120002");
@@ -50,6 +51,29 @@ public class Scraper{
 		}
 		System.out.println(response.body());
         return response.body();
+
+    }
+
+    public static String get_key(String run_id){
+        HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create("https://api.scrapemequickly.com/get-token?scraping_run_id=" + run_id))
+        .method("GET", HttpRequest.BodyPublishers.noBody())
+        .build();
+
+        HttpResponse<String> response = null;
+		try {
+			response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(response.body());
+        return response.body();
+
+    }
+
+    public static void get_data(String endpoint, int index, String key){
 
     }
 
